cf68763c49ab628f83a452a8ea570f01ccbcf377
Volters <kevinsvolters12@gmail.com>
2024-04-30 12:25:14 +0300

    Izveidoti vakances un aktualitates routes un views

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,19 +5,23 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('landing') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('vacancies')" :active="request()->routeIs('vacancies')">
+                        {{ __('Vakances') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('news')" :active="request()->routeIs('news')">
+                        {{ __('Aktualitates') }}
                     </x-nav-link>
                 </div>
             </div>
 
+            @if(!is_null(Auth::user()))
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
@@ -51,6 +55,8 @@
                     </x-slot>
                 </x-dropdown>
             </div>
+            @endif
+
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
@@ -67,13 +73,18 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('vacancies')" :active="request()->routeIs('vacancies')">
+                {{ __('Vakances') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('news')" :active="request()->routeIs('news')">
+                {{ __('Aktualitates') }}
             </x-responsive-nav-link>
         </div>
 
+        @if(!is_null(Auth::user()))
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
+
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
@@ -96,5 +107,6 @@
                 </form>
             </div>
         </div>
+        @endif
     </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('landing');
-});
+})->name('landing');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/vacancies', function () {
+    return view('vacancies');
+})->name('vacancies');
+
+Route::get('/news', function () {
+    return view('news');
+})->name('news');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
