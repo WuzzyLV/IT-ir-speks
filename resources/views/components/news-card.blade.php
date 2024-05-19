7ed8adbf48095d0e625778bf6488a77d4ff461f7
Volters <kevinsvolters12@gmail.com>
2024-05-19 20:19:13 +0300

    Added news-card and news section

---
+++ resources/views/components/news-card.blade.php
@@ -0,0 +1,32 @@
+<article
+    class="max-w m-6 mx-4 flex max-w-xl flex-col items-start justify-between"
+>   
+    
+    <img src="https://i1.sndcdn.com/artworks-EZSvIHkxvqSQ2jTU-z0oCGA-t500x500.jpg" class="mb-4 w-full h-64 object-fill rounded-2xl border border-gray-400 bg-gray-300">
+    
+
+    <div class="flex items-center gap-x-4 text-xs">
+        <time datetime="2020-03-16" class="text-gray-400">
+            2024. g. 19. maijs
+        </time>
+    </div>
+    
+    <div class="group relative">
+        <h3
+            class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600"
+        >
+            <a href="#">
+                <span class="absolute inset-0"></span>
+                Anglija izgudro 6G internetu
+            </a>
+        </h3>
+        <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+            Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam
+            vitae illo. Non aliquid explicabo necessitatibus unde. Sed
+            exercitationem placeat consectetur nulla deserunt vel. Iusto
+            corrupti dicta.
+        </p>
+    </div>
+    
+    
+</article>
--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,30 +1,24 @@
-<x-app-layout>
-    <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-            <h1 class="text-gray-900 font-bold text-center text-4xl m-4">Aktualitātes</h1>
-
-                <div class="p-6 text-gray-900 flex justify-evenly flex-wrap">
-                    
-
-                    <x-newscard />
-                    <x-newscard />
-                    <x-newscard />
-                    <x-newscard />
-                    <x-newscard />
-                    <x-newscard />
-
-
-                  
-                    
-
-                    
-
-
-                </div>
-            </div>
+<x-app-layout class="bg-light1 mt-10 sm:mt-16">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl lg:mx-0">
+            <h2
+                class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
+            >
+                Aktualitātes
+            </h2>
+            <p class="mt-2 text-lg leading-8 text-gray-600">
+                Uzzini, kas jauns un aktuāls IT nozarē visā pasaulē.
+            </p>
+        </div>
+        <div
+            class="mx-auto mt-10 grid max-w-2xl grid-cols-1 border-t border-gray-300 sm:mt-16 sm:gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+        >
+            <x-news-card />
+            <x-news-card />
+            <x-news-card />
+            <x-news-card />
+            <x-news-card />
+            <x-news-card />
         </div>
     </div>
-
-
 </x-app-layout>
