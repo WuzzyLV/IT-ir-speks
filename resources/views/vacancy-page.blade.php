262919db7d2632b7aae2ade509c03ce3f4f6ce4b
Volters <kevinsvolters12@gmail.com>
2024-05-19 22:35:29 +0300

    Added breadcrumbs to vacancies

--- resources/views/vacancy-page.blade.php
+++ resources/views/vacancy-page.blade.php
@@ -1,40 +1,37 @@
 <x-app-layout class="">
-    <div
-        class="bg-light1 relative isolate overflow-hidden px-6 py-20 sm:py-24 lg:overflow-visible lg:px-0"
-    >
+
+    <div class="bg-light1 relative isolate overflow-hidden px-6 py-20 sm:py-24 lg:overflow-visible lg:px-0">
+
         <div
-            class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10"
-        >
+            class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+
             <div
-                class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8"
-            >
+                class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+
                 <div class="lg:pr-4">
+                    <div class="text-sm breadcrumbs">
+                        <ul>
+                            <li><a href="{{route('landing')}}">Sākumlapa</a></li>
+                            <li><a href="{{route('vacancies')}}">Vakances</a></li>
+                            <li>Add Document</li>
+                        </ul>
+                    </div>
                     <div class="lg:max-w-lg">
-                        <p
-                            class="text-base font-semibold leading-7 text-indigo-600"
-                        >
+                        <p class="text-base font-semibold leading-7 text-indigo-600">
                             2024. g. 1. mar.
                         </p>
-                        <h1
-                            class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
-                        >
+                        <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                             Vecakais UML diagrammu specalists
                         </h1>
                     </div>
                 </div>
             </div>
             <div
-                class="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden"
-            >
+                class="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
                 <div>
-                    <div
-                        class="relative flex flex-col items-center gap-x-4 px-4 sm:px-0"
-                    >
-                        <img
-                            src="https://logos-world.net/wp-content/uploads/2020/06/Accenture-Emblem.png"
-                            alt=""
-                            class="h-10 w-10 rounded-full border border-gray-400 bg-gray-50 object-contain"
-                        />
+                    <div class="relative flex flex-col items-center gap-x-4 px-4 sm:px-0">
+                        <img src="https://logos-world.net/wp-content/uploads/2020/06/Accenture-Emblem.png" alt=""
+                            class="h-10 w-10 rounded-full border border-gray-400 bg-gray-50 object-contain" />
                         <div class="leading-6">
                             <p class="text-gray-900">
                                 <a href="#" class="">Accenture</a>
@@ -43,71 +40,42 @@
                     </div>
                     <div class="mt-6 border-t border-gray-100">
                         <dl class="divide-y divide-gray-100 text-center">
-                            <div
-                                class="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0"
-                            >
-                                <dt
-                                    class="text-sm font-medium leading-6 text-gray-900"
-                                >
+                            <div class="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
+                                <dt class="text-sm font-medium leading-6 text-gray-900">
                                     Slodze
                                 </dt>
-                                <dd
-                                    class="mt-1 text-sm leading-6 text-gray-700 sm:mt-0"
-                                >
+                                <dd class="mt-1 text-sm leading-6 text-gray-700 sm:mt-0">
                                     Pilna
                                 </dd>
                             </div>
-                            <div
-                                class="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0"
-                            >
-                                <dt
-                                    class="text-sm font-medium leading-6 text-gray-900"
-                                >
+                            <div class="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
+                                <dt class="text-sm font-medium leading-6 text-gray-900">
                                     Alga
                                 </dt>
-                                <dd
-                                    class="mt-1 text-sm leading-6 text-gray-700 sm:mt-0"
-                                >
+                                <dd class="mt-1 text-sm leading-6 text-gray-700 sm:mt-0">
                                     950 € - 1,110 €
                                 </dd>
                             </div>
-                            <div
-                                class="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0"
-                            >
-                                <dt
-                                    class="text-sm font-medium leading-6 text-gray-900"
-                                >
+                            <div class="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
+                                <dt class="text-sm font-medium leading-6 text-gray-900">
                                     Mājaslapa
                                 </dt>
-                                <dd
-                                    class="mt-1 text-sm leading-6 text-gray-700 sm:mt-0"
-                                >
-                                    <a
-                                        href="https://www.accenture.lv"
-                                        class="text-accent1 hover:underline"
-                                    >
+                                <dd class="mt-1 text-sm leading-6 text-gray-700 sm:mt-0">
+                                    <a href="https://www.accenture.lv" class="text-accent1 hover:underline">
                                         www.accenture.lv
                                     </a>
                                 </dd>
                             </div>
-                            <div
-                                class="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0"
-                            >
-                                <dt
-                                    class="text-sm font-medium leading-6 text-gray-900"
-                                >
+                            <div class="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
+                                <dt class="text-sm font-medium leading-6 text-gray-900">
                                     Pieteikšanās termiņš
                                 </dt>
-                                <dd
-                                    class="mt-1 text-sm leading-6 text-gray-700 sm:mt-0"
-                                >
+                                <dd class="mt-1 text-sm leading-6 text-gray-700 sm:mt-0">
                                     Līdz 2024. g. 15. mar.
                                 </dd>
                             </div>
                             <div class="flex justify-center px-4 py-6 sm:px-0">
-                                <a
-                                    class="hover:bg-dark1 bg-light1 btn btn-wide text-accent1"
-                                >
+                                <a class="hover:bg-dark1 bg-light1 btn btn-wide text-accent1">
                                     Pieteikties
                                 </a>
                             </div>
@@ -116,12 +84,9 @@
                 </div>
             </div>
             <div
-                class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8"
-            >
+                class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
                 <div class="lg:pr-4">
-                    <div
-                        class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg"
-                    >
+                    <div class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
                         <p>
                             Faucibus commodo massa rhoncus, volutpat. Dignissim
                             sed eget risus enim. Mattis mauris semper sed amet
@@ -171,9 +136,7 @@
                             adipiscing egestas. Iaculis convallis ac tempor et
                             ut. Ac lorem vel integer orci.
                         </p>
-                        <h2
-                            class="my-6 text-2xl font-bold tracking-tight text-gray-900"
-                        >
+                        <h2 class="my-6 text-2xl font-bold tracking-tight text-gray-900">
                             No server? No problem.
                         </h2>
                         <p class="mt-6">
