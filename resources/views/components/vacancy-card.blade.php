cdb71de197e6f844620c4f0d4a4165be375a6d5c
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-05-19 19:54:35 +0300

    Insane design

---
+++ resources/views/components/vacancy-card.blade.php
@@ -0,0 +1,54 @@
+<article
+    class="max-w m-6 mx-4 flex max-w-xl flex-col items-start justify-between"
+>
+    <div class="flex items-center gap-x-4 text-xs">
+        <time datetime="2020-03-16" class="text-gray-400">
+            Līdz 2024. g. 15. mar.
+        </time>
+        <a
+            href="#"
+            class="relative z-10 rounded-full bg-gray-200 px-3 py-1.5 font-medium text-gray-700 hover:bg-gray-300"
+        >
+            Pusslodze
+        </a>
+    </div>
+    <div class="group relative">
+        <h3
+            class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600"
+        >
+            <a href="#">
+                <span class="absolute inset-0"></span>
+                Vecakais UML diagrammu specalists
+            </a>
+        </h3>
+        <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+            Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam
+            vitae illo. Non aliquid explicabo necessitatibus unde. Sed
+            exercitationem placeat consectetur nulla deserunt vel. Iusto
+            corrupti dicta.
+        </p>
+    </div>
+    <div class="mt-6 flex w-full items-end justify-between">
+        <div class="relative flex items-center gap-x-4">
+            <img
+                src="https://logos-world.net/wp-content/uploads/2020/06/Accenture-Emblem.png"
+                alt=""
+                class="h-10 w-10 rounded-full border border-gray-400 bg-gray-50 object-contain"
+            />
+            <div class="leading-6">
+                <p class="text-gray-900">
+                    <a href="#" class="">Accenture</a>
+                </p>
+            </div>
+        </div>
+        <div>
+            <div class="stat py-0 pr-0">
+                <div class="stat-title text-lg">Alga</div>
+                <div class="stat-value text-sm text-gray-900 lg:text-lg">
+                    950 € - 1,110 €
+                </div>
+            </div>
+            <!-- <p class="font-semibold text-gray-900">950 € - 1,110 €</p> -->
+        </div>
+    </div>
+</article>
--- resources/views/vacancies.blade.php
+++ resources/views/vacancies.blade.php
@@ -1,12 +1,24 @@
-<x-app-layout>
-
-    <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("vakances") }}
-                </div>
-            </div>
+<x-app-layout class="bg-light1 mt-10 sm:mt-16">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl lg:mx-0">
+            <h2
+                class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
+            >
+                Vakances
+            </h2>
+            <p class="mt-2 text-lg leading-8 text-gray-600">
+                Atrodi savu sapņa darba vietu.
+            </p>
+        </div>
+        <div
+            class="mx-auto mt-10 grid max-w-2xl grid-cols-1 border-t border-gray-300 sm:mt-16 sm:gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+        >
+            <x-vacancy-card />
+            <x-vacancy-card />
+            <x-vacancy-card />
+            <x-vacancy-card />
+            <x-vacancy-card />
+            <x-vacancy-card />
         </div>
     </div>
 </x-app-layout>
