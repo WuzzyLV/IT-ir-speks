9b047118c50b03c2270dbfb9176c86b9b67f8080
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-05-21 00:03:50 +0300

    Make progress on the users view

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,73 +1,29 @@
-<x-staff-layout class="flex justify-center">
-    <div class="m-4 max-w-7xl px-6 lg:px-8">
-        <div class="mx-auto lg:mx-0">
-            <div class="flex justify-between">
-                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                    Lietotāji
-                </h2>
-                <a href="#" class="btn bg-transparent text-accent1 border-accent">Jauns lietotājs</a>
+<x-staff-layout class="w-full text-gray-900">
+    <div
+        class="mb-4 flex items-center justify-between border-b border-gray-300 px-6 py-4 shadow lg:px-8"
+    >
+        <h2 class="tracking-tightsm:text-xl text-lg font-bold">Lietotāji</h2>
+        <a
+            href="#"
+            class="btn btn-sm border-accent bg-transparent text-accent1"
+        >
+            Jauns lietotājs
+        </a>
+    </div>
+    <div>
+        <div class="stat max-w-48">
+            <div class="stat-figure text-lg text-accent1">
+                <i class="fas fa-users"></i>
             </div>
-
-            <p class="mt-2 text-lg leading-8 text-gray-600">
-                Saraksts ar administratoriem un moderatoriem.
-            </p>
+            <div class="stat-title text-gray-400">Lietotāji</div>
+            <div class="stat-value my-2">8</div>
         </div>
-        
-        <div class="stats shadow bg-light1 ">
-
-            <div class="stat">
-                <div class="stat-figure text-primary">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                        class="inline-block w-8 h-8 stroke-current">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z">
-                        </path>
-                    </svg>
-                </div>
-                <div class="stat-title">Total Likes</div>
-                <div class="stat-value text-primary">25.6K</div>
-                <div class="stat-desc">21% more than last month</div>
+        <div class="stat max-w-48">
+            <div class="stat-figure text-lg text-accent1">
+                <i class="fas fa-users"></i>
             </div>
-
-            <div class="stat">
-                <div class="stat-figure text-secondary">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                        class="inline-block w-8 h-8 stroke-current">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M13 10V3L4 14h7v7l9-11h-7z"></path>
-                    </svg>
-                </div>
-                <div class="stat-title">Page Views</div>
-                <div class="stat-value text-secondary">2.6M</div>
-                <div class="stat-desc">21% more than last month</div>
-            </div>
-
-            <div class="stat">
-                <div class="stat-figure text-secondary">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                        class="inline-block w-8 h-8 stroke-current">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M13 10V3L4 14h7v7l9-11h-7z"></path>
-                    </svg>
-                </div>
-                <div class="stat-title">Page Views</div>
-                <div class="stat-value text-secondary">2.6M</div>
-                <div class="stat-desc">21% more than last month</div>
-            </div>
-
-            <div class="stat">
-                <div class="stat-figure text-secondary">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                        class="inline-block w-8 h-8 stroke-current">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M13 10V3L4 14h7v7l9-11h-7z"></path>
-                    </svg>
-                </div>
-                <div class="stat-title">Page Views</div>
-                <div class="stat-value text-secondary">2.6M</div>
-                <div class="stat-desc">21% more than last month</div>
-            </div>
-
+            <div class="stat-title text-gray-400">Pēdējā aktivitāte</div>
+            <div class="stat-value my-2">Reinis izveidoja vakanci</div>
         </div>
     </div>
 </x-staff-layout>
