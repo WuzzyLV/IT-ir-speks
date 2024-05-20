b9c578a1039bf14f848d2c130c69dfb8f082c610
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-05-21 00:03:36 +0300

    improve staff layout

--- resources/views/layouts/staff.blade.php
+++ resources/views/layouts/staff.blade.php
@@ -2,50 +2,57 @@
 
 <!DOCTYPE html>
 <html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
+        <title>{{ config("app.name", "Laravel") }}</title>
 
-    <title>{{ config("app.name", "Laravel") }}</title>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net" />
+        <link
+            href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap"
+            rel="stylesheet"
+        />
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+            integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+        />
+        <!-- Scripts -->
+        @vite(["resources/css/app.css", "resources/js/app.js"])
+    </head>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net" />
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- Scripts -->
-    @vite(["resources/css/app.css", "resources/js/app.js"])
-</head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include("layouts.navigation", ["isAdmin" => true])
 
-<body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100">
-        @include("layouts.navigation", ["isAdmin" => true])  
-
-        <!-- Impliment left nav -->
-        <div class="drawer lg:drawer-open">
-            <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-            <main class="drawer-content {{$class}}">
-                <!-- Page content here -->
-                
-                {{$slot}}
-                
-            </main>
-            <div class="drawer-side">
-                <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
-                <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-                    <!-- Sidebar content here -->
-                    <li><a href="{{route("dashboard")}}">Dashy</a></li>
-                    <li><a href="{{route("users")}}">Lietotāji</a></li>
-                    <li><a>Sidebar Item 2</a></li>
-                </ul>
+            <!-- Impliment left nav -->
+            <div class="drawer lg:drawer-open">
+                <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+                <main class="{{ $class }} drawer-content bg-light1">
+                    <!-- Page content here -->
+                    {{ $slot }}
+                </main>
+                <div class="drawer-side">
+                    <label
+                        for="my-drawer-2"
+                        aria-label="close sidebar"
+                        class="drawer-overlay"
+                    ></label>
+                    <ul
+                        class="menu min-h-full w-72 border-r border-gray-300 bg-light1 p-4 text-gray-900"
+                    >
+                        <!-- Sidebar content here -->
+                        <li><a href="{{ route("dashboard") }}">Dashy</a></li>
+                        <li><a href="{{ route("users") }}">Lietotāji</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
-
-        
-    </div>
-</body>
-
+    </body>
 </html>
