06ad3f26556eb9e1cbe5146900912676dcec4e76
Volters <kevinsvolters12@gmail.com>
2024-05-23 11:34:33 +0300

    Fixed padding

--- resources/views/news-page.blade.php
+++ resources/views/news-page.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="bg-light1 relative isolate overflow-hidden px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+    <div class="bg-light1 relative isolate overflow-hidden px-6 py-8 sm:py-32 lg:overflow-visible lg:px-0">
         <div
             class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
             <div
