132d2dc326f427a094f98353a620cbe95ebfd8ec
Volters <kevinsvolters12@gmail.com>
2024-05-23 11:29:25 +0300

    Fixed vacancy page form

--- resources/views/vacancy-page.blade.php
+++ resources/views/vacancy-page.blade.php
@@ -27,7 +27,7 @@
                 </div>
             </div>
             <div
-                class="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden"
+                class=" -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden"
             >
                 <div id="preview">
                     <div
