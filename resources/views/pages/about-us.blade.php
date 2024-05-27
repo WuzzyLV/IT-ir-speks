338c818b9a5735ac271d55b483e557bfa99f5c75
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-05-27 11:56:45 +0300

    Contact us

--- resources/views/pages/about-us.blade.php
+++ resources/views/pages/about-us.blade.php
@@ -1,35 +1,44 @@
 <x-app-layout class="mt-10 bg-light1 sm:mt-16">
-
     <body class="font-inter">
-
-
-        <section class="py-14 lg:py-24 relative z-0 ">
-            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative text-center">
+        <section class="relative z-0 py-14 lg:py-24">
+            <div
+                class="relative mx-auto max-w-7xl px-4 text-center sm:px-6 lg:px-8"
+            >
                 <h1
-                    class="max-w-2xl mx-auto text-center font-manrope font-bold text-4xl  text-gray-900 mb-5 md:text-5xl md:leading-normal">
-                    Atrodi sev piemērotu darbu IT ar mūsu piedāvātajām <span class="text-accent1">vakancēm </span>
+                    class="font-manrope mx-auto mb-5 max-w-2xl text-center text-4xl font-bold text-gray-900 md:text-5xl md:leading-normal"
+                >
+                    Atrodi sev piemērotu darbu IT ar mūsu piedāvātajām
+                    <span class="text-accent1">vakancēm</span>
                 </h1>
-
-
             </div>
         </section>
 
-        <section class="py-14 lg:py-24 relative">
-            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative ">
-                <div class="grid grid-cols-1 lg:grid-cols-2 gap-9">
+        <section class="relative py-14 lg:py-24">
+            <div class="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div class="grid grid-cols-1 gap-9 lg:grid-cols-2">
                     <div class="img-box">
-                        <img src="https://pagedone.io/asset/uploads/1702034769.png" alt="About Us tailwind page"
-                            class="max-lg:mx-auto">
+                        <img
+                            src="https://pagedone.io/asset/uploads/1702034769.png"
+                            alt="About Us tailwind page"
+                            class="max-lg:mx-auto"
+                        />
                     </div>
-                    <div class="lg:pl-[100px] flex items-center">
+                    <div class="flex items-center lg:pl-[100px]">
                         <div class="data w-full">
                             <h2
-                                class="font-manrope font-bold text-4xl lg:text-5xl text-black mb-9 max-lg:text-center relative">
-                                Par mums </h2>
-                            <p class="font-normal text-xl leading-8 text-gray-500 max-lg:text-center max-w-2xl mx-auto">
-                                Mēs esam specializēta aģentūra, kas apvieno talantīgus IT profesionāļus ar izcilām darba
-                                iespējām visā Latvijā. Mūsu mērķis ir nodrošināt, ka katrs mūsu klients, gan darba
-                                meklētāji, gan darba devēji, saņem augstākās kvalitātes pakalpojumus un atbalstu.
+                                class="font-manrope relative mb-9 text-4xl font-bold text-black max-lg:text-center lg:text-5xl"
+                            >
+                                Par mums
+                            </h2>
+                            <p
+                                class="mx-auto max-w-2xl text-xl font-normal leading-8 text-gray-500 max-lg:text-center"
+                            >
+                                Mēs esam specializēta aģentūra, kas apvieno
+                                talantīgus IT profesionāļus ar izcilām darba
+                                iespējām visā Latvijā. Mūsu mērķis ir
+                                nodrošināt, ka katrs mūsu klients, gan darba
+                                meklētāji, gan darba devēji, saņem augstākās
+                                kvalitātes pakalpojumus un atbalstu.
                             </p>
                         </div>
                     </div>
@@ -37,61 +46,97 @@
             </div>
         </section>
 
-        <section class="py-14 lg:py-24 relative">
-            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative ">
-                <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-9 ">
-
-                    <div class="lg:pr-24 flex items-center">
+        <section class="relative py-14 lg:py-24">
+            <div class="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-9">
+                    <div class="flex items-center lg:pr-24">
                         <div class="data w-full">
-                            <img src="https://pagedone.io/asset/uploads/1702034785.png" alt="About Us tailwind page"
-                                class="block lg:hidden mb-9 mx-auto">
-                            <h2 class="font-manrope font-bold text-4xl lg:text-5xl text-black mb-9 max-lg:text-center">
-                                Mūsu misija</h2>
-                            <p class="font-normal text-xl leading-8 text-gray-500 max-lg:text-center max-w-2xl mx-auto">
-                                Mūsu misija ir veicināt IT nozares attīstību Latvijā, palīdzot uzņēmumiem atrast
-                                vispiemērotākos speciālistus un palīdzot IT profesionāļiem atrast darbu, kas atbilst
-                                viņu prasmēm un ambīcijām. Mēs ticam, ka IT ir spēks, kas var mainīt pasauli, un mūsu
-                                uzdevums ir nodrošināt, ka šis spēks tiek izmantots visefektīvākajā veidā.
+                            <img
+                                src="https://pagedone.io/asset/uploads/1702034785.png"
+                                alt="About Us tailwind page"
+                                class="mx-auto mb-9 block lg:hidden"
+                            />
+                            <h2
+                                class="font-manrope mb-9 text-4xl font-bold text-black max-lg:text-center lg:text-5xl"
+                            >
+                                Mūsu misija
+                            </h2>
+                            <p
+                                class="mx-auto max-w-2xl text-xl font-normal leading-8 text-gray-500 max-lg:text-center"
+                            >
+                                Mūsu misija ir veicināt IT nozares attīstību
+                                Latvijā, palīdzot uzņēmumiem atrast
+                                vispiemērotākos speciālistus un palīdzot IT
+                                profesionāļiem atrast darbu, kas atbilst viņu
+                                prasmēm un ambīcijām. Mēs ticam, ka IT ir spēks,
+                                kas var mainīt pasauli, un mūsu uzdevums ir
+                                nodrošināt, ka šis spēks tiek izmantots
+                                visefektīvākajā veidā.
                             </p>
                         </div>
                     </div>
-                    <div class="img-box ">
-                        <img src="https://pagedone.io/asset/uploads/1702034785.png" alt="About Us tailwind page"
-                            class="hidden lg:block ">
+                    <div class="img-box">
+                        <img
+                            src="https://pagedone.io/asset/uploads/1702034785.png"
+                            alt="About Us tailwind page"
+                            class="hidden lg:block"
+                        />
                     </div>
                 </div>
             </div>
         </section>
-
-
-
 
         <footer class="w-full">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-
-                <div class=" py-10 max-md:max-w-sm max-md:mx-auto m-16">
-                    <div class="col-span-full mb-10 lg:col-span-2 lg:mb-0 flex items-center flex-col">
-                        <h1 class="text-gray-900 text-2xl font-bold py-4">
+                <div class="m-16 py-10 max-md:mx-auto max-md:max-w-sm">
+                    <div
+                        class="lg:mb-0s col-span-full mb-10 flex flex-col items-center gap-y-4 lg:col-span-2"
+                    >
+                        <h1 class="py-4 text-2xl font-bold text-gray-900">
                             Sazinies ar mums!
                         </h1>
-                        <div class="flex justify-center items-center flex-row">
+                        <p class="max-w-96 pb-4 text-center">
+                            Vari droši sazināties ar mums jebkādos jautājumos,
+                            gan tehniskos, gan parastu palīdzības jautāšanu
+                        </p>
+                        <div
+                            class="flex flex-row items-center justify-center gap-x-4"
+                        >
                             <i class="fa-solid fa-envelope text-accent1"></i>
-                            <p class="py-1 text-xl text-gray-500 lg:max-w-xs text-center lg:text-left">itirspeks@lvt.lv
-                            </p>
+                            <a
+                                href="mailto:itirspeks@lvt.lv"
+                                target="_blank"
+                                class="py-1 text-center text-xl text-gray-500 hover:underline lg:max-w-xs lg:text-left"
+                            >
+                                itirspeks@lvt.lv
+                            </a>
                         </div>
-                        <div>
+                        <div
+                            class="flex flex-row items-center justify-center gap-x-4 hover:underline"
+                        >
                             <i class="fa-solid fa-phone text-accent1"></i>
-                            <p class="py-1 text-xl text-gray-500 lg:max-w-xs text-center lg:text-left">+371 112</p>
+                            <a
+                                href="tel:+37122848121"
+                                target="_blank"
+                                class="py-1 text-center text-xl text-gray-500 lg:max-w-xs lg:text-left"
+                            >
+                                +371 22 848 121
+                            </a>
                         </div>
-
+                        <div
+                            class="flex flex-row items-center justify-center gap-x-4 hover:underline"
+                        >
+                            <i class="fa-solid fa-building text-accent1"></i>
+                            <a
+                                href="https://www.google.com/maps/place/57%C2%B013'05.7%22N+21%C2%B040'52.5%22E/@57.2182449,21.6786607,17z/data=!3m1!4b1!4m13!1m8!3m7!1s0x46f1d2b68a0dd139:0x400cfcd68f320a0!2sPiltene,+Piltenes+pils%C4%93ta,+Ventspils+novads,+LV-3620!3b1!8m2!3d57.2241305!4d21.6744137!16zL20vMGI3cG01!3m3!8m2!3d57.218242!4d21.681241?entry=ttu"
+                                target="_blank"
+                                class="py-1 text-center text-xl text-gray-500 lg:max-w-xs lg:text-left"
+                            >
+                                Piltenes, Sīļu iela 52
+                            </a>
+                        </div>
                     </div>
-
-
-
-
                 </div>
-
-
             </div>
         </footer>
     </body>
