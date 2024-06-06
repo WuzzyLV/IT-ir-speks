37071e4b5fe7e003ba5c7cd32a8fae1d5ecdaa99
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-06-06 18:39:30 +0300

    Disabling delete btn for root

--- resources/views/components/admin/user-row.blade.php
+++ resources/views/components/admin/user-row.blade.php
@@ -1,4 +1,9 @@
 @props(['user'])
+<?php
+use App\Roles;
+$role = $user->role()->get()[0];
+$isRoot= Roles::from($role->name) == Roles::Root;
+?>
 <tr class="border-b-0  my-4">
     <td class="text-center">
         <p class="mb-1 text-base font-medium">
@@ -7,7 +12,7 @@
         <p class="text-gray-400">{{$user->email}}</p>
     </td>
     <td class="hidden justify-center xs:flex ">
-        <x-role-badge :role="$user->role()->get()[0]"/>
+        <x-role-badge :role="$role"/>
     </td>
     <td>
         <div class="flex items-center justify-center gap-x-2">
@@ -16,13 +21,23 @@
                     class="fa-solid fa-pencil text-accent1"
                 ></i>
             </a>
-            <a class="btn btn-circle btn-outline btn-sm shadow">
-                <i
-                    class="fa-solid fa-trash-can text-red-500"
-                ></i>
-            </a>
+
+            @if($isRoot)
+                <a class="btn btn-disabled btn-circle btn-outline btn-sm shadow">
+                    <i
+                        class="fa-solid fa-trash-can text-red-400"
+                    ></i>
+                </a>
+                @else
+                <a href=""
+                    class="btn btn-circle btn-outline btn-sm shadow">
+                    <i
+                        class="fa-solid fa-trash-can text-red-500"
+                    ></i>
+                </a>
+            @endif
+
         </div>
     </td>
 </tr>
 
-{{--{{var_dump($user)}}--}}
