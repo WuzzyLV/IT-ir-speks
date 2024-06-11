c08fa02c6f6b49e8828591212e3473a9daf661b4
Volters <kevinsvolters12@gmail.com>
2024-06-12 00:39:07 +0300

    Added vacancy-row

--- resources/views/pages/admin/vacancies.blade.php
+++ resources/views/pages/admin/vacancies.blade.php
@@ -1,7 +1,7 @@
 <x-staff-layout class="w-full flex flex-col text-gray-900">
     <div class=" flex items-center justify-between border-b border-gray-300 px-6 py-4 shadow lg:px-8">
         <h2 class="tracking-tight sm:text-xl text-lg font-bold">Vakances</h2>
-        <a href="{{route("edit-vacancy", 1)}}" class="btn btn-sm border-accent1 bg-transparent text-accent1">
+        <a href="{{route("new-vacancy", 1)}}" class="btn btn-sm border-accent1 bg-transparent text-accent1">
             Jauna vakance
         </a>
     </div>
@@ -17,91 +17,9 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <!-- row 1 -->
-                    <tr class="border-b-0 my-4">
-                        <td class="text-center">
-                            <p class="mb-1 text-base font-medium">
-                                Vecakais UML diagrammu specalists
-                            </p>
-
-                        </td>
-                        <td class="hidden justify-center items-center xs:flex ">
-                            <div class="relative flex items-center gap-x-4">
-                                <img src="https://logos-world.net/wp-content/uploads/2020/06/Accenture-Emblem.png" alt="" class="h-10 w-10 rounded-full  bg-gray-50 object-contain" />
-                                <div class="leading-6">
-                                    <p class="text-">
-                                        <a href="#" class="">Accenture</a>
-                                    </p>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="flex items-center justify-center gap-x-2">
-                                <a class="btn btn-circle btn-outline btn-sm shadow">
-                                    <i class="fa-solid fa-pencil text-accent1"></i>
-                                </a>
-                                <a class="btn btn-circle btn-outline btn-sm shadow">
-                                    <i class="fa-solid fa-trash-can text-red-500"></i>
-                                </a>
-                        </td>
-                    </tr>
-                    <!-- row 2 -->
-                    <tr class="border-b-0 my-4">
-                        <td class="text-center">
-                            <p class="mb-1 text-base font-medium">
-                                Vecakais UML diagrammu specalists
-                            </p>
-
-                        </td>
-                        <td class="hidden justify-center items-center xs:flex ">
-                            <div class="relative flex items-center gap-x-4">
-                                <img src="https://logos-world.net/wp-content/uploads/2020/06/Accenture-Emblem.png" alt="" class="h-10 w-10 rounded-full  bg-gray-50 object-contain" />
-                                <div class="leading-6">
-                                    <p class="text-">
-                                        <a href="#" class="">Accenture</a>
-                                    </p>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="flex items-center justify-center gap-x-2">
-                                <a class="btn btn-circle btn-outline btn-sm shadow">
-                                    <i class="fa-solid fa-pencil text-accent1"></i>
-                                </a>
-                                <a class="btn btn-circle btn-outline btn-sm shadow">
-                                    <i class="fa-solid fa-trash-can text-red-500"></i>
-                                </a>
-                        </td>
-                    </tr>
-                    <!-- row 3 -->
-                    <tr class="border-b-0 my-4">
-                        <td class="text-center">
-                            <p class="mb-1 text-base font-medium">
-                                Vecakais UML diagrammu specalists
-                            </p>
-
-                        </td>
-                        <td class="hidden justify-center items-center xs:flex ">
-                            <div class="relative flex items-center gap-x-4">
-                                <img src="https://logos-world.net/wp-content/uploads/2020/06/Accenture-Emblem.png" alt="" class="h-10 w-10 rounded-full  bg-gray-50 object-contain" />
-                                <div class="leading-6">
-                                    <p class="text-">
-                                        <a href="#" class="">Accenture</a>
-                                    </p>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="flex items-center justify-center gap-x-2">
-                                <a class="btn btn-circle btn-outline btn-sm shadow">
-                                    <i class="fa-solid fa-pencil text-accent1"></i>
-                                </a>
-                                <a class="btn btn-circle btn-outline btn-sm shadow">
-                                    <i class="fa-solid fa-trash-can text-red-500"></i>
-                                </a>
-                        </td>
-                    </tr>
-
+                   @foreach(\App\Models\Vacancy::all() as $vacancy)
+                        <x-admin.vacancy-row :vacancy="$vacancy"/>
+                    @endforeach
                 </tbody>
             </table>
         </div>
