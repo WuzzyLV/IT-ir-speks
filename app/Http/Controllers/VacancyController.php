f26ccd09a04a4930521374e0485cafd94ca51b48
Volters <kevinsvolters12@gmail.com>
2024-06-11 23:16:47 +0300

    Added handleNew function

--- app/Http/Controllers/VacancyController.php
+++ app/Http/Controllers/VacancyController.php
@@ -2,9 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
+use App\Models\Vacancy;
+use App\Roles;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+use Illuminate\View\View;
 
 class VacancyController extends Controller
 {
-    //
+    public function new(Request $request): View
+    {
+        return view('pages.admin.forms.edit-vacancy', [
+            'user' => null,
+            'new' => true,
+        ]);
+    }
+
+    public function handleNew(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'company' => 'required',
+            'desc' => 'required',
+            'website' => 'required',
+            // 'file_id' => 'required',
+            'city' => 'required',
+            'workload' => 'required',
+            'salary' => 'required',
+            'deadline' => 'required',
+        ]);
+
+        if ($request->hasFile('image')) {
+            $request->validate([
+                'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:20480',
+            ]);
+
+        }
+
+        $vacancy = new Vacancy();
+        $vacancy->name = $request->name;
+        $vacancy->company = $request->company;
+        $vacancy->desc = $request->desc;
+        $vacancy->website = $request->website;
+        // $vacancy->file_id = $request->file_id;
+        $vacancy->city = $request->city;
+        $vacancy->workload = $request->workload;
+        $vacancy->salary = $request->salary;
+        $vacancy->deadline = $request->deadline;
+        $vacancy->save();
+
+        // return redirect()->route('admin-vacancies');
+    }
+
 }
