b71c4f76d745f7e508e3c71b2282f52b61031f37
Volters <kevinsvolters12@gmail.com>
2024-06-12 00:37:57 +0300

    Added vacancy-row

---
+++ resources/views/components/admin/vacancy-row.blade.php
@@ -0,0 +1,29 @@
+@props(['vacancy'])
+
+<tr class="border-b-0 my-4">
+    <td class="text-center">
+        <p class="mb-1 text-base font-medium">
+            {{$vacancy->name}}
+        </p>
+
+    </td>
+    <td class="hidden justify-center items-center xs:flex ">
+        <div class="relative flex items-center gap-x-4">
+            <img src="https://logos-world.net/wp-content/uploads/2020/06/Accenture-Emblem.png" alt="" class="h-10 w-10 rounded-full  bg-gray-50 object-contain" />
+            <div class="leading-6">
+                <p class="text-">
+                    <a href="#" class="">{{$vacancy->company}}</a>
+                </p>
+            </div>
+        </div>
+    </td>
+    <td>
+        <div class="flex items-center justify-center gap-x-2">
+            <a class="btn btn-circle btn-outline btn-sm shadow">
+                <i class="fa-solid fa-pencil text-accent1"></i>
+            </a>
+            <a class="btn btn-circle btn-outline btn-sm shadow">
+                <i class="fa-solid fa-trash-can text-red-500"></i>
+            </a>
+    </td>
+</tr>
