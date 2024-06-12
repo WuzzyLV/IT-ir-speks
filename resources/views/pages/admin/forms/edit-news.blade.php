725b7a42ec5a995ccef33fb85c8467fb1cf17603
Volters <kevinsvolters12@gmail.com>
2024-06-13 00:16:43 +0300

    Added required to all inputs

--- resources/views/pages/admin/forms/edit-news.blade.php
+++ resources/views/pages/admin/forms/edit-news.blade.php
@@ -53,8 +53,8 @@
                                     type="text"
                                     name="title"
                                     id="title"
-                                    required
                                     placeholder="Virsraksts"
+                                    required
                                     class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent1 sm:text-sm sm:leading-6"
                                     value="{{ $news ? $news->title : "" }}"
                                 />
@@ -87,6 +87,7 @@
                                     accept="image/*"
                                     autocomplete="new-image"
                                     placeholder="attels"
+                                    required
                                     class="ml-1"
                                 />
                             </div>
