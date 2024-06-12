c69d49a91958ef34c4835f9ee4922476ac2aeadb
Volters <kevinsvolters12@gmail.com>
2024-06-12 09:43:46 +0300

    Fixed textarea

--- resources/views/pages/admin/forms/edit-news.blade.php
+++ resources/views/pages/admin/forms/edit-news.blade.php
@@ -57,8 +57,7 @@
                                 Mazais apraksts
                             </label>
                             <div class="mt-2">
-                                <textarea type="text" name="desc" id="desc" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent1 sm:text-sm sm:leading-6 bg-white pl-3 h-40">
-                                </textarea>
+                                <textarea type="text" name="desc" id="desc" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent1 sm:text-sm sm:leading-6 bg-white pl-4 h-40"></textarea>
                             </div>
                         </div>
 
