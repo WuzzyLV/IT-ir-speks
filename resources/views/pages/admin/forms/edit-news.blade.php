c2684f6296b6c7cfc5238d46f1f074c6ffe9144f
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-06-12 09:38:28 +0300

    chore: Add missing closing div tag in edit-news form template

--- resources/views/pages/admin/forms/edit-news.blade.php
+++ resources/views/pages/admin/forms/edit-news.blade.php
@@ -16,6 +16,7 @@
                 Saglabat
             </button>
         </div>
+        </div>
         {{--show errors--}}
         @if ($errors->any())
         <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative text-center" role="alert">
