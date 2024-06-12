8128d8e0349a8e05fc73feef4799e4f77d81c053
Volters <kevinsvolters12@gmail.com>
2024-06-12 09:57:06 +0300

    Added edit news article

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -24,7 +24,7 @@ class NewsController extends Controller
             'content' => 'required',
             'desc' => 'required'
         ]);
-        echo "here";
+        
 
         if ($request->hasFile('image')) {
             $request->validate([
@@ -51,14 +51,29 @@ class NewsController extends Controller
             'new' => false,
         ]);
     }
-    public function handleEdit(Request $request)
+    public function handleEdit(Request $request): RedirectResponse
     {
-        $news = News::find($request->id);
-
-        return view('pages.admin.forms.edit-news', [
-            'news' => $news,
-            'new' => false,
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+            'desc' => 'required'
         ]);
+        
+
+        if ($request->hasFile('image')) {
+            $request->validate([
+                'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:20480',
+            ]);
+
+        }
+
+        $news = News::find($request->id);
+        $news->title = $request->title;
+        $news->content = $request->content;
+        $news->desc = $request->desc;
+        $news->save();
+
+       return redirect()->route('admin-news');
     }
 
     public function destroy(Request $request): RedirectResponse
--- resources/views/pages/admin/forms/edit-news.blade.php
+++ resources/views/pages/admin/forms/edit-news.blade.php
@@ -56,6 +56,7 @@
                                     required
                                     placeholder="Virsraksts"
                                     class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent1 sm:text-sm sm:leading-6"
+                                    value="{{$news ? $news->title : ""}}"
                                 />
                             </div>
                         </div>
@@ -81,13 +82,14 @@
                                     autocomplete="new-image"
                                     placeholder="attels"
                                     class="ml-1"
+                                    
                                 />
                             </div>
                         </div>
 
                         <div class="col-span-full">
                             <label
-                                for="title"
+                                for="desc"
                                 class="block text-sm font-medium leading-6 text-gray-900"
                             >
                                 Apraksts
@@ -99,7 +101,8 @@
                                     id="desc"
                                     required
                                     class="block h-40 w-full rounded-md border-0 bg-white py-1.5 pl-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent1 sm:text-sm sm:leading-6"
-                                ></textarea>
+                                    
+                                >{{$news ? $news->desc : "Gang GAng GAN"}}</textarea>
                             </div>
                         </div>
 
