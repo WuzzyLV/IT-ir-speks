60d0222e3903cbf3f9c578d318747db9110a531e
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-06-12 15:31:23 +0300

    Blade utils for date finally

---
+++ app/Helpers/BladeUtils.php
@@ -0,0 +1,35 @@
+<?php
+
+class BladeUtils {
+
+    static array $dates= [
+        1 =>"janvāris",
+        2=>"februāris",
+        3=>"marts",
+        4=>"aprīlis",
+        5=>"maijs",
+        6=>"jūnijs",
+        7=>"jūlijs",
+        8=>"augusts",
+        9=>"septembris",
+        10=>"oktobris",
+        11=>"novembris",
+        12=>"decembris"
+    ];
+
+    public static function formatDate($date, $short = false): string
+    {
+        $date = new DateTime($date);
+        $month = $date->format('n');
+        $day = $date->format('j');
+        $year = $date->format('Y');
+
+        $formattedMonth = self::$dates[$month];
+        if ($short) {
+            //cut off at 3 char
+            $formattedMonth=substr($formattedMonth, 0, 3);
+        }
+        // 2024. g. 19. maijs
+        return "$year. g. $day. $formattedMonth";
+    }
+}
