a6a45522081dae80ed72958e72da8c8b4acf506f
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-06-12 16:00:46 +0300

    make link work on news card

--- resources/views/components/news-card.blade.php
+++ resources/views/components/news-card.blade.php
@@ -23,7 +23,7 @@
         <h3
             class="text-lg text-{{ $h3Color }} mt-3 font-semibold leading-6 group-hover:text-accent1"
         >
-            <a href="{{ route("news-page", 1) }}">
+            <a href="{{ route("news-page", $news->id) }}">
                 <span class="absolute inset-0"></span>
                 {{ $news->title }}
             </a>
