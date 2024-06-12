193315c71a927f49eb04da59aeadbcd0ce422353
Volters <kevinsvolters12@gmail.com>
2024-06-12 15:35:46 +0300

    News index

--- resources/views/components/news-card.blade.php
+++ resources/views/components/news-card.blade.php
@@ -1,32 +1,25 @@
-@props(["textColor" => "", "h3Color" => ""])
-<article
-    class="max-w m-6 mx-4 flex max-w-xl flex-col items-start justify-between"
->
-    <img
-        src="https://i1.sndcdn.com/artworks-EZSvIHkxvqSQ2jTU-z0oCGA-t500x500.jpg"
-        class="mb-4 h-64 w-full rounded-2xl bg-gray-950/25 object-cover"
-    />
+@props(["textColor" => "", "h3Color" => "", "news"])
+
+@isset($news)
+<article class="max-w m-6 mx-4 flex max-w-xl flex-col items-start justify-between">
+    <img src="https://i1.sndcdn.com/artworks-EZSvIHkxvqSQ2jTU-z0oCGA-t500x500.jpg" class="mb-4 h-64 w-full rounded-2xl bg-gray-950/25 object-cover" />
 
     <div class="flex items-center gap-x-4 text-xs">
-        <time datetime="2020-03-16" class="text-gray-400">
-            2024. g. 19. maijs
+        <time datetime="{{ $news->created_at->format("Y. \g. j. F") }}">
+            {{ $news->created_at->format("Y. \g. j. F") }}
         </time>
     </div>
 
     <div class="group relative">
-        <h3
-            class="text-lg text-{{ $h3Color }} mt-3 font-semibold leading-6 group-hover:text-accent1"
-        >
-            <a href="{{ route("news-page", 1) }}">
+        <h3 class="text-lg text-{{ $h3Color }} mt-3 font-semibold leading-6 group-hover:text-accent1">
+            <a href="{{ route('news-page', 1) }}">
                 <span class="absolute inset-0"></span>
-                Anglijā izgudro 6G internetu
+                {{ $news->title }}
             </a>
         </h3>
         <p class="text-{{ $textColor }} mt-5 line-clamp-3 text-sm leading-6">
-            Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam
-            vitae illo. Non aliquid explicabo necessitatibus unde. Sed
-            exercitationem placeat consectetur nulla deserunt vel. Iusto
-            corrupti dicta.
+            {{ $news->desc }}
         </p>
     </div>
 </article>
+@endisset
