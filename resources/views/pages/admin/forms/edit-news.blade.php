b02d1dff538e4d849442e9e2fbc71d5b3e4fc682
Volters <kevinsvolters12@gmail.com>
2024-06-12 09:35:47 +0300

    Comment

--- resources/views/pages/admin/forms/edit-news.blade.php
+++ resources/views/pages/admin/forms/edit-news.blade.php
@@ -34,7 +34,8 @@
                                 <input type="text" name="title" id="title" required placeholder="Virsraksts" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent1 sm:text-sm sm:leading-6" />
                             </div>
                         </div>
-
+                        
+                        <!-- image -->
                         <div class="sm:col-span-3">
                             <label for="image" class="block text-sm font-medium leading-6 text-gray-900">
                                 Attēls
@@ -65,7 +66,7 @@
 
                         
 
-                        <!-- image -->
+                        
                         
 
                         
