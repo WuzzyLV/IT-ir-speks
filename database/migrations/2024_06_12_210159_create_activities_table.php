be0babc90a7ec4668bc750ba380d9711f301a52f
Volters <kevinsvolters12@gmail.com>
2024-06-13 00:06:05 +0300

    Renamed table activity to activities

--- database/migrations/2024_06_12_210159_create_activities_table.php
+++ database/migrations/2024_06_12_210159_create_activities_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('activity', function (Blueprint $table) {
+        Schema::create('activities', function (Blueprint $table) {
             $table->id();
             $table->foreignId('who')->references('id')->on('users');
             $table->string('desc');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('activity');
+        Schema::dropIfExists('activities');
     }
 };
