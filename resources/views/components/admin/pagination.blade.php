d0e0028530638f4344d90bebc518f252e11eafc8
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-06-13 23:39:05 +0300

    pagination keeps filters if exist

--- resources/views/components/admin/pagination.blade.php
+++ resources/views/components/admin/pagination.blade.php
@@ -2,11 +2,23 @@
 
 <div class="join border-gray-300">
     <form>
+        @if(request()->has('workload'))
+        <input type="hidden" name="workload" value="{{request()->workload}}">
+        @endif
+        @if(request()->has('city'))
+        <input type="hidden" name="city" value="{{request()->city}}">
+        @endif
         <input type="hidden" name="page" value="{{$page-1}}">
         <button {{$page-1==0 ? "disabled" : "" }} class="{{$page-1==0 ? "btn-disabled" : "" }} join-item btn bg-transparent btn-sm !rounded-l-lg !text-gray-900  !border-gray-300 hover:bg-gray-300/50">«</button>
     </form>
     <button class="join-item btn bg-transparent btn-sm !text-gray-900 !border-gray-300 hover:bg-gray-300/50 px-6 pointer-events-none	">{{$page}}</button>
     <form class="inline-block">
+        @if(request()->has('workload'))
+            <input type="hidden" name="workload" value="{{request()->workload}}">
+        @endif
+        @if(request()->has('city'))
+            <input type="hidden" name="city" value="{{request()->city}}">
+        @endif
         <input type="hidden" name="page" value="{{$page+1}}">
         <button class="{{$page+1>$totalPages ? "btn-disabled" : "" }} join-item btn bg-transparent btn-sm !rounded-r-lg !text-gray-900 !border-gray-300 hover:bg-gray-300/50">»</button>
     </form>
