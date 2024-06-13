74c991cccb3d69b286568c1118a44b4bd7548bcb
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-06-13 23:04:29 +0300

    remove bs blue accents

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -2,5 +2,5 @@
 
 <input
     {{ $disabled ? "disabled" : "" }}
-    {!! $attributes->merge(["class" => "border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"]) !!}
+    {!! $attributes->merge(["class" => "border-gray-300 focus:border-accent1 focus:ring-accent1 rounded-md shadow-sm"]) !!}
 />
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -60,7 +60,7 @@
 
                         <button
                             form="send-verification"
-                            class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-accent1 focus:ring-offset-2"
                         >
                             {{ __("Click here to re-send the verification email.") }}
                         </button>
